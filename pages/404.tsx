7af7d1aff7ab03ef2fa3205466d924b2188196af
Valker Vieira <valker.vieira@gmail.com>
2021-10-17 10:28:17 -0300

    add custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { H1 } from '@components/Text';
+
+export const Custom404 = () => {
+  return (
+    <div>
+      <H1>Ta perdido, parcero?</H1>
+    </div>
+  );
+};
+
+export default Custom404;
