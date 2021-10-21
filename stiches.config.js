ef289bdd03a16ab78e55a8f614d3807810d9ea5c
Valker Vieira <valker.vieira@gmail.com>
2021-10-21 01:30:58 -0300

    cleanup stiches.config.js

--- stiches.config.js
+++ stiches.config.js
@@ -14,20 +14,6 @@ export const {
       hiContrast: 'hsl(206,10%,5%)',
       loContrast: 'white',
 
-      gray100: 'hsl(206,22%,99%)',
-      gray200: 'hsl(206,12%,97%)',
-      gray300: 'hsl(206,11%,92%)',
-      gray400: 'hsl(206,10%,84%)',
-      gray500: 'hsl(206,10%,76%)',
-      gray600: 'hsl(206,10%,44%)',
-
-      purple100: 'hsl(252,100%,99%)',
-      purple200: 'hsl(252,100%,98%)',
-      purple300: 'hsl(252,100%,94%)',
-      purple400: 'hsl(252,75%,84%)',
-      purple500: 'hsl(252,78%,60%)',
-      purple600: 'hsl(252,80%,53%)',
-
       //
       // white
       //
@@ -35,9 +21,13 @@ export const {
       //
       // black
       //
-      black400: 'hsl(206, 26%, 27%)',
-      black500: 'hsl(206, 72%, 11%)',
-      black600: 'hsl(0, 0%, 0%)',
+      black500: 'hsl(207, 26%, 14%)',
+      black600: 'hsl(207, 26%, 7%)',
+      //
+      // gray
+      //
+      gray400: 'hsl(206, 26%, 27%)',
+      gray500: 'hsl(207, 26%, 27%)',
       //
       // yellow
       //
