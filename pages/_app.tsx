186dda1fb6e5a4c7215804989542ab728a19c734
Valker Vieira <valker.vieira@gmail.com>
2021-10-22 04:11:22 -0300

    Feature/basic navigation and identity (#3)
    * update favicon and add basic navigation
    
    * update logo
    
    * remove <main> tag
    
    * small update to styles and create layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps } from 'next/app';
 import { Session } from 'next-auth';
 import { Provider as SessionProvider } from 'next-auth/client';
+import { reset } from 'stitches-reset';
+import { globalCss } from 'stiches.config';
+import { Layout } from '@templates/Layout';
+
+const globalStyles = globalCss(reset, { body: { background: '$white' } });
 
 export const MyApp: (props: AppProps<{ session: Session }>) => JSX.Element = ({
   Component,
   pageProps: { session, ...pageProps },
 }): JSX.Element => {
+  globalStyles();
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 };
