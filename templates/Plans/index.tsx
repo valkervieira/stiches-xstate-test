6fb8a0fb5a79b140def0eb110c34095a4b220b6c
Valker Vieira <valker.vieira@gmail.com>
2021-10-24 02:21:54 -0300

    fix plans template bug

--- templates/Plans/index.tsx
+++ templates/Plans/index.tsx
@@ -1,10 +1,5 @@
 import { H1 } from '@components/Text';
-import { Layout } from '@templates/Layout';
 
 export const PlansTemplate = (): JSX.Element => {
-  return (
-    <Layout>
-      <H1>Plans page</H1>
-    </Layout>
-  );
+  return <H1>Plans page</H1>;
 };
